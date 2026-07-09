bb4b56ac674e2ef1256cd6e0d959934c93a88e3f
Ab-Coder123 <abdoalmaestro123za@gmail.com>
2026-07-09 15:44:46 +0300

    new code

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"1IsUbo2yTrVvEJHSwpeyqPHQAEgKJNiHTOtN1Cnxv+M=\"}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"V/p6KpW0ojSSd5rYPkftaw1beJDFt3YA2OFvQfpdIXI=\"\n}"
